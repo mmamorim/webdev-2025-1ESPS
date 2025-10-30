f7eb712a5480a981e0a93b88923ef7be861d8a8c
Marcelo <marc.m.amorim@gmail.com>
2025-10-29 21:02:57 -0300

    v1

--- Aula10_29OUT/main.js
+++ Aula10_29OUT/main.js
@@ -3,12 +3,21 @@ console.log("oi gente");
 
 function cifrarAtbash(texto) {
     let alfaMaiusculo = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    let alfaMinusculo = "abcdefghijklmnopqrstuvwxyz"
     let saida = ""
-    for(let i=0; i<texto.length; i++) {
+    for (let i = 0; i < texto.length; i++) {
         //console.log("letra: ",texto[i]);
         let letterIn = texto[i]
-        let pos = alfaMaiusculo.search(letterIn)
-        let letterOut = alfaMaiusculo[26-pos-1]           
+        let letterOut = " "
+        if (letterIn != " ") {
+            let pos = alfaMaiusculo.search(letterIn)
+            if (pos != -1) {
+                letterOut = alfaMaiusculo[26 - pos - 1]
+            } else {
+                pos = alfaMinusculo.search(letterIn)
+                letterOut = alfaMinusculo[26 - pos - 1]
+            }
+        }
         //console.log("letraOut: ",letterOut);
         saida = saida + letterOut
     }
@@ -16,5 +25,43 @@ function cifrarAtbash(texto) {
     return saida
 }
 
-let cifrado = cifrarAtbash("ALFACE")
-console.log("cifrado",cifrado);
+let cifrado = cifrarAtbash("Al face")
+console.log("cifrado", cifrado);
+let decifrado = cifrarAtbash(cifrado)
+console.log("decifrado", decifrado);
+
+
+function gerarChavesRSA_Didaticas(p, q) {
+    if (p <= 1 || q <= 1) return null;
+
+    const N = p * q;
+    const phi_N = (p - 1) * (q - 1);
+
+    let E = 3;
+    while (E < phi_N) {
+        // Encontrar o primeiro E que é coprimo de phi_N
+        if ((phi_N % E !== 0) && ((p - 1) % E !== 0) && ((q - 1) % E !== 0)) {
+            // Otimização: A verificação (p-1)%E e (q-1)%E não é rigorosamente a do RSA, 
+            // mas é didática e evita fatores óbvios para primos pequenos.
+            break;
+        }
+        E++;
+    }
+
+    let D = 1;
+    while (D < phi_N) {
+        // Encontrar D tal que (D * E) % phi_N === 1
+        if ((D * E) % phi_N === 1) {
+            break;
+        }
+        D++;
+    }
+
+    return {
+        publica: { E, N }, // Use E e N para CIFRAR
+        privada: { D, N }  // Use D e N para DECIFRAR
+    };
+}
+
+let resp = gerarChavesRSA_Didaticas(13,17)
+console.log(resp);
